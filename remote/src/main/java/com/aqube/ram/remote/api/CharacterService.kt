7619802c7dceb8c0ab29c83192da7d6ae8c8e117
Akhilesh <akhilesh0707@gmail.com>
2021-04-22 23:07:25 +0530

    Change query parameter to path in service interface

--- remote/src/main/java/com/aqube/ram/remote/api/CharacterService.kt
+++ remote/src/main/java/com/aqube/ram/remote/api/CharacterService.kt
@@ -3,13 +3,13 @@ package com.aqube.ram.remote.api
 import com.aqube.ram.remote.models.CharacterModel
 import com.aqube.ram.remote.models.CharacterResponseModel
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface CharacterService {
 
     @GET("character")
     suspend fun getCharacters(): CharacterResponseModel
 
-    @GET("character/")
-    suspend fun getCharacter(@Query("id") id: Long): CharacterModel
+    @GET("character/{id}")
+    suspend fun getCharacter(@Path("id") id: Long): CharacterModel
 }
